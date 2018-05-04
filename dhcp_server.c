95d29daf140e8e416836b1d196f94e08dda56af8
John Salvador <jmsalvador2395@gmail.com>
2018-05-03 21:09:02 -0500

    fixed the recvfrom() error, cleaned up printing, and started reading more of the discover packet

--- dhcp_server.c
+++ dhcp_server.c
@@ -5,7 +5,7 @@
 
 #pragma comment(lib,"ws2_32.lib") //winsock library
 
-#define BUFLEN 512
+#define BUFLEN 1024
 int main(int argc, char *argv[]){
 	int server_fd;
 	SOCKET server_skt;
@@ -13,9 +13,12 @@ int main(int argc, char *argv[]){
 	struct sockaddr_in server, si_other;
 	unsigned char buffer[BUFLEN];
 	WSADATA wsa;
+	int error;
 
+	slen=sizeof(si_other);
 	printf("Initialising Winsock...\n\n");
 	if(WSAStartup(MAKEWORD(2,2),&wsa)!=0){
+
 		printf("Failed. Error Code: %d",WSAGetLastError());
 		return 1;
 	}
@@ -23,7 +26,8 @@ int main(int argc, char *argv[]){
 	printf("Winsock Initialised\n\n");
 
 	if((server_skt=socket(AF_INET, SOCK_DGRAM, 0))==INVALID_SOCKET){
-		printf("Creating socket failed: %d\n", WSAGetLastError());
+		error=WSAGetLastError();
+		printf("Creating socket failed: %d\n", error);
 		return 1;
 	}
 	printf("Socket created\n\n");
@@ -33,7 +37,8 @@ int main(int argc, char *argv[]){
 	server.sin_port=htons(67);
 
 	if(bind(server_skt,(struct sockaddr *)&server, sizeof(server))==SOCKET_ERROR){
-		printf("Bind failed. Error code: %d\n",WSAGetLastError());
+		error=WSAGetLastError();
+		printf("Bind failed. Error code: %d\n",error);
 		exit(EXIT_FAILURE);
 	}
 	puts("Bind done");
@@ -44,36 +49,84 @@ int main(int argc, char *argv[]){
 
 		memset(buffer,'\0',BUFLEN);
 		if((recv_len=recvfrom(server_skt, buffer, BUFLEN,0, (struct sockaddr *) &si_other, &slen))==SOCKET_ERROR){
-			printf("recvfrom() failed. Error code: %d\n", WSAGetLastError());
+			error=WSAGetLastError();
+			printf("recvfrom() failed. Error code: %d\n", error);
 			exit(EXIT_FAILURE);
 		}
 		else{
-			printf("******************************************************************\n\n");
-			printf("Received packet from %s:%d\n",inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+			printf("Received packet from %s:%d\n\n",inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+			printf("***********************************************************************************************************************\n\n");
 			int index=0;
-			printf("OP: %x\n", buffer[index++]);
-			printf("HTYPE: %x\n", buffer[index++]);
-			printf("HLEN: %x\n", buffer[index++]);
-			printf("HOPS: %x\n", buffer[index++]);
-			printf("XID: %x %x %x %x\n",buffer[index],buffer[index+1],buffer[index+2],buffer[index+3]);
-			index+=4;
-			printf("SECS: %x %X\n",buffer[index],buffer[index+1]);
+			printf("OP:\n\t%02x\n", buffer[index++]);
+
+			printf("HTYPE:\n\t%02x\n", buffer[index++]);
+
+			printf("HLEN:\n\t%02x\n", buffer[index++]);
+
+			printf("HOPS:\n\t%02x\n", buffer[index++]);
+
+			int endloop=index+4;
+			printf("XID:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+
+			printf("Seconds Elapsed:\n\t%02x %02x\n",buffer[index],buffer[index+1]);
 			index+=2;
-			printf("FLAGS: %x %x\n",buffer[index], buffer[index+1]);
+
+			printf("FLAGS:\n\t%02x %02x\n",buffer[index], buffer[index+1]);
 			index+=2;
-			printf("Client IP: %x %x %x %x\n",buffer[index],buffer[index+1],buffer[index+2],buffer[index+3]);
-			index+=4;
-			printf("Server IP: %x %x %x %x\n",buffer[index],buffer[index+1],buffer[index+2],buffer[index+3]);
-			index+=4;
-			printf("Gateway IP: %x %x %x %x\n",buffer[index++],buffer[index++],buffer[index++],buffer[index++]);
-			printf("Client HW Address: \n\t%x %x %x %x\n\t%x %x %x %x\n\t%x %x %x %x\n\t%x %x %x %x\n"
-					,buffer[index],buffer[index+1],buffer[index+2],buffer[index+3]
-					,buffer[index+4],buffer[index+5],buffer[index+6],buffer[index+7]
-					,buffer[index+8],buffer[index+9],buffer[index+10],buffer[index+11]
-					,buffer[index+12],buffer[index+13],buffer[index+14],buffer[index+15]
-				  );
-			index+=16;
-			printf("******************************************************************\n\n");
+
+			endloop=index+4;
+			printf("Client IP:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+
+			endloop=index+4;
+			printf("Your (client) IP:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+
+			endloop=index+4;
+			printf("Next Server IP:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+
+			endloop=index+4;
+			printf("Relay Agent IP:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+	
+			endloop=index+16;
+			printf("Client MAC Address:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+			
+			endloop=index+80;
+			printf("Server Host Name:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+		
+			endloop=index+144;
+			printf("Boot File Name:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+			
+			endloop=index+4;
+			printf("Magic Cookie:\n\t");
+			while(index<endloop)
+				printf("%02x ",buffer[index++]);
+			printf("\n");
+
+
+			printf("***********************************************************************************************************************\n\n");
 		}
 	}
 	return 0;
