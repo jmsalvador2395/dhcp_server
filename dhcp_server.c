08a69a473c8df7b92703f6a7ba5c4587e88d2323
John Salvador <jmsalvador2395@gmail.com>
2018-05-15 17:51:20 -0500

    moved the message reading into a function and added some constants

--- dhcp_server.c
+++ dhcp_server.c
@@ -6,6 +6,14 @@
 #pragma comment(lib,"ws2_32.lib") //winsock library
 
 #define BUFLEN 1024
+#define SERVER_IP 0xc0a80101 //your ip (192.168.1.1)
+#define NETMASK 0xffffff00 //netmask (255.255.255.0)
+
+#define END 0xff
+
+void proccess_client_message(unsigned char *buff);
+
+void send_offer_message(
 int main(int argc, char *argv[]){
 	int server_fd;
 	SOCKET server_skt;
@@ -56,90 +64,97 @@ int main(int argc, char *argv[]){
 		else{
 			printf("Received packet from %s:%d\n\n",inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
 			printf("***********************************************************************************************************************\n\n");
-			int index=0;
-			printf("Message Type:\n\t%02x\n", buffer[index++]);
+			proccess_client_message(buffer);
 
-			printf("Hardware Type:\n\t%02x\n", buffer[index++]);
-
-			printf("Hardware Address Length:\n\t%02x\n", buffer[index++]);
-
-			printf("Hops:\n\t%02x\n", buffer[index++]);
-
-			int endloop=index+4;
-			printf("Transaction ID:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-
-			printf("Seconds Elapsed:\n\t%02x %02x\n",buffer[index],buffer[index+1]);
-			index+=2;
-
-			printf("Flags:\n\t%02x %02x\n",buffer[index], buffer[index+1]);
-			index+=2;
-
-			endloop=index+4;
-			printf("Client IP:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-
-			endloop=index+4;
-			printf("Your (client) IP:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-
-			endloop=index+4;
-			printf("Next Server IP:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-
-			endloop=index+4;
-			printf("Relay Agent IP:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-	
-			endloop=index+16;
-			printf("Client MAC Address:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-			
-			endloop=index+64;
-			printf("Server Host Name:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-		
-			endloop=index+128;
-			printf("Boot File Name:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-			
-			endloop=index+4;
-			printf("Magic Cookie:\n\t");
-			while(index<endloop)
-				printf("%02x ",buffer[index++]);
-			printf("\n");
-
-			unsigned char option;
-			
-			option=buffer[index++];
-			while(option!=0xff){
-				printf("Option: %02x\n",option);
-				int length=0|buffer[index++];
-				printf("\tLength: %d\n",length);
-				for(int i=0;i<length;i++)
-					printf("\t\t%02x\n",buffer[index++]);
-				option=buffer[index++];
-			}
-			printf("Option: %02x (end)\n",option);
-			printf("***********************************************************************************************************************\n\n");
 		}
 	}
 	return 0;
 	
+}
+
+void proccess_client_message(unsigned char *buff){
+	int index=0;
+	printf("Message Type:\n\t%02x\n", buff[index++]);
+
+	printf("Hardware Type:\n\t%02x\n", buff[index++]);
+
+	printf("Hardware Address Length:\n\t%02x\n", buff[index++]);
+
+	printf("Hops:\n\t%02x\n", buff[index++]);
+
+	int endloop=index+4;
+	printf("Transaction ID:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+
+	printf("Seconds Elapsed:\n\t%02x %02x\n",buff[index],buff[index+1]);
+	index+=2;
+
+	printf("Flags:\n\t%02x %02x\n",buff[index], buff[index+1]);
+	index+=2;
+
+	endloop=index+4;
+	printf("Client IP:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+
+	endloop=index+4;
+	printf("Your (client) IP:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+
+	endloop=index+4;
+	printf("Next Server IP:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+
+	endloop=index+4;
+	printf("Relay Agent IP:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+
+	endloop=index+16;
+	printf("Client MAC Address:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+	
+	endloop=index+64;
+	printf("Server Host Name:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+
+	endloop=index+128;
+	printf("Boot File Name:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+	
+	endloop=index+4;
+	printf("Magic Cookie:\n\t");
+	while(index<endloop)
+		printf("%02x ",buff[index++]);
+	printf("\n");
+
+	unsigned char option;
+	
+	option=buff[index++];
+	while(option!=0xff){
+		printf("Option: %02x\n",option);
+		int length=0|buff[index++];
+		printf("\tLength: %d\n",length);
+		for(int i=0;i<length;i++)
+			printf("\t\t%02x\n",buff[index++]);
+		option=buff[index++];
+	}
+	printf("Option: %02x (end)\n",option);
+	printf("***********************************************************************************************************************\n\n");
+
+	return;
 }
