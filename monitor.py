9c39f7a93d394ddf2825c841b6bd4ce71869a27a
jmsalvador2395 <jmsalvador2395@gmail.com>
2019-08-23 10:55:42 -0700

    made a short program to monitor network traffic

---
+++ monitor.py
@@ -0,0 +1,15 @@
+import socket
+
+if __name__ == '__main__':
+	HOST='127.0.0.1'
+	PORT=68 #should be 68
+	BUFLEN=1024
+
+	skt=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	skt.bind(('', PORT))
+
+	while True:
+		print('Listening for messages on ' + HOST + ':' + str(PORT))
+		data, addr = skt.recvfrom(BUFLEN)
+		print('received message:\n'+str(data))
+	
